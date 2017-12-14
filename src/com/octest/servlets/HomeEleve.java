2d5a418daa07cf08c8ed83d30bce9edba9c05583
Gregoire0505 <gregoire@pasquier-desvignes.com>
2017-12-14 14:54:20 +0100

    Modifs HomeEleve

--- src/com/octest/servlets/HomeEleve.java
+++ src/com/octest/servlets/HomeEleve.java
@@ -10,28 +10,58 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.octest.beans.Utilisateur;
-import com.octest.form.ConnexionForm;
+import com.octest.beans.*;
+import com.octest.form.*;
 
-
-public class HomeEleve {
+@WebServlet("/HomeEleve")
+public class HomeEleve extends HttpServlet {
+//public class HomeEleve {
 	/* Take the id of student */
-	 public static final String ACCES_UTILISATEUR  = "/WEB-INF/Acceuil_Eleve.jsp";
+	
+	public static final String VUE ="/WEB-INF/View/Accueil_Eleve.jsp";
+	public static final String ATT_SESSION_USER = "sessionUser";
+	public static final String ATT_USER ="utilisateur";
+	public static final String ATT_FORM ="form";
+	
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+	}
+	 
+	/***public static final String ACCES_UTILISATEUR  = "/WEB-INF/View/Accueil_Eleve.jsp";
 	 public static final String ATT_SESSION_USER = "sessionUser";
 
 	    protected void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException {
-	        /* Affichage de la page restreinte */
+	        // Affichage de la page restreinte 
 	        this.getServletContext().getRequestDispatcher( ACCES_UTILISATEUR ).forward( request, response );
-	    } 
+	    } ***/
 	    
-		private ServletRequest getServletContext() {
+		/**private ServletRequest getServletContext() {
 			// TODO Auto-generated method stub
 			return null;
-		}
+		}**/
 		
 		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 			
 			/* We create a new object form*/
-	        this.getServletContext().getRequestDispatcher( ACCES_UTILISATEUR ).forward( request, response );
+			ConnexionForm form = new ConnexionForm();
+			
+			Utilisateur utilisateur = form.connecterUtilisateur(request);
+			
+			HttpSession session = request.getSession();
+			
+			if(form.getErreurs().isEmpty()) {
+				session.setAttribute(ATT_SESSION_USER, utilisateur);
+			}
+			else {
+				session.setAttribute(ATT_SESSION_USER, null);
+				
+			}
+			request.setAttribute(ATT_FORM, form);
+			request.setAttribute(ATT_USER, utilisateur);
+			
+			 /* Transmission de la paire d'objets request/response à notre JSP */
+	        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
 		}
+
 }
